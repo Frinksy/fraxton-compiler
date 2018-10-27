de1e78acb90cdff8370436afac0ea6941f141fd5
Adam B <mondeobr@gmail.com>
2018-10-27 13:23:33 +0100

    created tokenizer.cpp

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -4,4 +4,17 @@
 
 using namespace std;
 
+/*
+
+
+    int bool char string array --> data types
+    = --> assignment
+    == <= >= 
+    { } ( ) [ ] -->
+    ; --> end of statement
+    sjdsfisfowc23__xw1 --> identifier
+
+
+*/
+
 
