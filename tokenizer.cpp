132da5bc8165ea9abae98139da956e52d841112f
Adam B <mondeobr@gmail.com>
2018-10-27 20:51:44 +0100

    fixed symbols not being split at the end of a file added comments throughout word_separator.cpp

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -14,6 +14,31 @@ using namespace std;
 
 */
 
+bool isInteger (string word)
+{
+    for (unsigned i = 0; i < word.size(); i++)
+    {
+        if (!isdigit(word[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isDefiner (string word)
+{
+    string definers[] = {"int", "char", "string", "bool", "array", "float"};
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (word == definers[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 struct token
 {
@@ -24,7 +49,32 @@ struct token
 
 vector<token> assign_tokens (vector<string> words) 
 {
-
     vector<token> output;
+    for (unsigned i = 0; i < words.size(); i++) 
+   {    
+       token t;
+       t.seq = words[i];
+       // DEBUG : 
+        cout << endl << "Token is : " << t.seq << endl;
+
+        if (isInteger(words[i]))
+        {
+            t.type = "integer";
+        }
+        else if (isDefiner(words[i]))
+        {
+            t.type = "definer";
+        }
+        else 
+        {
+            t.type = "undefined";
+        }
+        cout << "Type is : " << t.type << endl;
+        
+
+    }
+
+
+
     return output;
 }
